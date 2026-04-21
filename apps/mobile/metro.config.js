d8ee3b1d9533f49e3123c9efbfc0278339fcb1ce
MahmoudAbdAlKareem <mabdalkareem@restaurant365.com>
2026-04-21 15:18:31 +0300

    Add docker screen

---
+++ apps/mobile/metro.config.js
@@ -0,0 +1,16 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(projectRoot);
+
+// Let Metro see packages hoisted to the monorepo root node_modules
+config.watchFolders = [workspaceRoot];
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+
+module.exports = config;
